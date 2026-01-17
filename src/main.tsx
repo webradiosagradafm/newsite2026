0415f11d51e5eb2404e6d9e55a693d6597ca0ec3
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-17 21:03:59 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-
-import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from './registerSW';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
+registerSW();
 
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
