ca01fa02ac0a0c7b632d56db67fdc79254bf19f8
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-17 21:14:25 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    fontWeight: {
+      normal: "400",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
+  corePlugins: {
+    fontWeight: false, // 🚨 desativa font-bold, font-medium etc
   },
   plugins: [],
-}
+};
