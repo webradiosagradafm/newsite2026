747e5e7e176daea25b86537be9e5e0efba60f3a6
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-17 18:55:01 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // 👈 ESSENCIAL para Codespaces / GitHub / Vercel preview
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
+    emptyOutDir: true
   }
-});
+})
