21b588ef22d87761c59a90b8031f92cde61b57c2
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-17 19:03:10 +0000

    update

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', () => {
-  console.log('SW installed')
-})
-
-self.addEventListener('fetch', () => {})
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Praise FM USA</h1>
-      <p>Vite + React funcionando ✅</p>
-    </div>
-  )
-}
