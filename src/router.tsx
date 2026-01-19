d12522f2c119fdfdcf3e3dbe46da13a9dc4976b3
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-19 05:26:31 +0000

    update

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import AppHome from './pages/AppHome'
-import SiteHome from './pages/Home'
+import SiteHome from './pages/AppHomePage'  // Alterado para AppHomePage
 
 export const router = createBrowserRouter([
   { path: '/', element: <SiteHome /> },
