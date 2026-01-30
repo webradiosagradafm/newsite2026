683dea32cc023e9c687a97046aca8c290f32c4a1
Praise FM Media <webradiosagradafm@gmail.com>
2026-01-30 06:58:03 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { registerSW } from './registerSW';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-registerSW();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
