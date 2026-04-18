bcb3196475bd5b469782651bccb509e999a07b8f
Praise FM United States <webradiosagradafm@gmail.com>
2026-04-18 20:46:15 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -11,16 +11,18 @@ export default defineConfig({
       manifest: {
         name: 'Praise FM USA',
         short_name: 'Praise FM',
-        description: 'Praise FM USA Radio',
-        theme_color: '#ff6600',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
         icons: [
           {
-            src: 'icon-192.png',
+            src: '/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: '/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
