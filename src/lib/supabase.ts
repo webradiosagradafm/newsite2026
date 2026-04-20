e08ff4c57e779b09911b3697cee8f279cc7cc4de
Praise FM United States <webradiosagradafm@gmail.com>
2026-04-19 22:04:53 -0300

    Update supabase.ts
    Signed-off-by: Praise FM United States <webradiosagradafm@gmail.com>

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
-
+// supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zhxickunddldjdusalfq.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_xFf78d86OQqGf8EkGpOqAQ_HIEmW92f';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
