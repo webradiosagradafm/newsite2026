f08cf74a27461b762d87862f8254d1641fd47c68
Praise FM United States <webradiosagradafm@gmail.com>
2026-04-20 05:19:50 +0000

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -12,5 +12,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    storageKey: 'praisefm-auth',
   },
 })
